8873cfd2b88a60ad6b6272d438fc3c37b3ee1276
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-04-13 16:00:46 -0400

    refactor button className with classnames lib

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,11 @@
 import React from "react";
 
 import "components/Button.scss";
+import classNames from 'classnames';
 
 export default function Button(props) {
-   let buttonClass = "button";
-
-   if (props.confirm) {
-      buttonClass += " button--confirm";
-   } else if (props.danger) {
-      buttonClass += " button--danger";
-   }
+   const { confirm, danger } = props;
+   const buttonClass = classNames("button", { "button--confirm": confirm }, { "button--danger": danger });
 
    return <button className={buttonClass} disabled={props.disabled} onClick={props.onClick}>{props.children}</button>;
 
