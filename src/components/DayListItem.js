cba88add693ec9c1ff7d29ed5c03c579079bf0d1
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-04-13 18:12:06 -0400

    format text for spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,10 +5,14 @@ import classNames from 'classnames';
 export default function DayListItem(props) {
   const listClass = classNames("day-list__item", { "day-list__item--selected": props.selected }, { "day-list__item--full": props.spots === 0 });
 
+  const formatSpots = (spots) => {
+    return `${spots === 0 ? "no" : spots} spot${spots === 1 ? "" : "s"} remaining`;
+  };
+
   return (
     <li className={listClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
