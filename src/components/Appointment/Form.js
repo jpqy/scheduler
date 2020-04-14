0461943f6b121671d31b2afc297f9869ec997522
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-04-14 17:29:09 -0400

    use hooks for name and interviewer in Form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,6 +3,9 @@ import Button from "../Button";
 import InterviewerList from "../InterviewerList";
 
 export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -12,12 +15,11 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-          /*
-            This must be a controlled component
-          */
+            value={name}
+            onChange={(event) => setName(event.target.value)}
           />
         </form>
-        <InterviewerList interviewers={props.interviewers} value={props.interviewer} onChange={props.setInterviewer} />
+        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -8,7 +8,7 @@ export default function InterviewerList(props) {
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
-        {props.interviewers.map(interviewer => <InterviewerListItem key={interviewer.id} setInterviewer={()=>props.onChange(interviewer.id)} name={interviewer.name} avatar={interviewer.avatar} selected={props.value===interviewer.id} />)}
+        {props.interviewers.map(interviewer => <InterviewerListItem key={interviewer.id} setInterviewer={() => props.onChange(interviewer.id)} name={interviewer.name} avatar={interviewer.avatar} selected={props.value === interviewer.id} />)}
       </ul>
     </section>
   );
