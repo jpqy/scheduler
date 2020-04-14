f63c652cf2d9b6b1f68a703714bbdea7ddef5d4c
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-04-14 14:41:40 -0400

    rename variables to value and onChange

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,7 +1,6 @@
 import React from "react";
 import 'components/InterviewerList.scss';
 import InterviewerListItem from 'components/InterviewerListItem';
-import classNames from 'classnames';
 
 export default function InterviewerList(props) {
 
@@ -9,7 +8,7 @@ export default function InterviewerList(props) {
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
-        {props.interviewers.map(interviewer => <InterviewerListItem key={interviewer.id} setInterviewer={()=>props.setInterviewer(interviewer.id)} name={interviewer.name} avatar={interviewer.avatar} selected={props.interviewer===interviewer.id} />)}
+        {props.interviewers.map(interviewer => <InterviewerListItem key={interviewer.id} setInterviewer={()=>props.onChange(interviewer.id)} name={interviewer.name} avatar={interviewer.avatar} selected={props.value===interviewer.id} />)}
       </ul>
     </section>
   );
