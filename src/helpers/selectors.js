db624f8309ec6527042285d499f4d8b2a67c6c44
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-04-15 15:25:25 -0400

    finish getAppointmentsForDay selector

---
+++ src/helpers/selectors.js
@@ -0,0 +1,19 @@
+// The function will return an array of appointments for the given day.
+export function getAppointmentsForDay(state, dayString) {
+
+  // Find the array of appointment IDs of the query day
+  let appointmentIdArray = [];
+  for (const day of state.days) {
+    if (day.name === dayString) {
+      appointmentIdArray = day.appointments;
+    }
+  }
+
+  // Add each appointment with that key
+  const filteredAppointments = [];
+  for (const appointmentId of appointmentIdArray) {
+    filteredAppointments.push(state.appointments[appointmentId.toString()]);
+  }
+
+  return filteredAppointments;
+}
