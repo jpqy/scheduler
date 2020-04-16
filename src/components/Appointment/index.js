c3042e39953115a92923a3f5a9f4a00ae410ff74
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-04-16 17:11:37 -0400

    "create" view shows form and back button works

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,10 +3,12 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 import useVisualMode from "hooks/useVisualMode";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
 
@@ -15,12 +17,15 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={props.onAdd} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
+      )}
+      {mode === CREATE && (
+        <Form interviewers={[]} onCancel={() => back()} />
       )}
     </article>
   );
