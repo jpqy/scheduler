0ee3a1ad0820ae8642643321badc3ba14e4f91a6
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-04-16 16:20:04 -0400

    implement transition and back functions

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode) => {
+    setMode(newMode);
+    setHistory(prevArray => [...prevArray, newMode]);
+  };
+
+
+  const back = () => {
+    setHistory(prevArray => {
+      prevArray.pop();
+      setMode(prevArray[prevArray.length - 1]);
+      return prevArray;
+    });
+    
+    // Why didn't the code below work? After calling setHistory, the value of history seems unchanged
+    // console.log("before setHistory", history);
+    // setHistory(history.slice(0, -1));
+    // console.log("after setHistory", history);
+    // setMode(history[history.length - 1]);
+  };
+
+
+
+  return { mode, transition, back };
+}
