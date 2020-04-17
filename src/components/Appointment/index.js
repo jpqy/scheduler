8a5d7dcb0295ee3860444975c28f1651e9277a4f
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-04-17 13:24:50 -0400

    use Status while saving

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,11 +4,13 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 import useVisualMode from "hooks/useVisualMode";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
 
 export default function Appointment(props) {
 
@@ -19,6 +21,7 @@ export default function Appointment(props) {
       student: name,
       interviewer
     };
+    transition(SAVING, true);
     props.bookInterview(props.id, interview)
       .then(() => transition(SHOW));
   }
@@ -35,6 +38,9 @@ export default function Appointment(props) {
       )}
       {mode === CREATE && (
         <Form interviewers={props.interviewers} onCancel={() => back()} onSave={save} />
+      )}
+      {mode === SAVING && (
+        <Status message={"Saving"} />
       )}
     </article>
   );
