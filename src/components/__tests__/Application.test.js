01f8dff5188153ab3e94bb2ede3fa022981b29ee
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-04-21 15:56:16 -0400

    test calls put req on saving Form

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -19,13 +19,24 @@ describe("Application", () => {
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
     const { container } = render(<Application />);
 
+    // Wait for mock axios data to load
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
+    // Get the first article tag using testId
     const appointments = getAllByTestId(container, "appointment");
-
     const firstAppointment = appointments[0];
-    console.log(prettyDOM(firstAppointment));
+
+    // Click the add button
+    fireEvent.click(getByAltText(firstAppointment, "Add"));
+
+    // Type in a name and select an interviewer
+    fireEvent.change(getByPlaceholderText(firstAppointment, "Enter Student Name"), {
+      target: { value: "Test Student" }
+    });
+    fireEvent.click(getByAltText(firstAppointment, "Sylvia Palmer"));
+
+    // Click "save"
+    fireEvent.click(getByText(firstAppointment, "Save"));
   });
 
 });
-
