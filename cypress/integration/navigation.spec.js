c3b9d83082dbfe49c4b5b0ce619c14f5b0738ea9
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-04-22 15:15:43 -0400

    cypress test for selecting new day

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,4 +2,13 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    cy.contains("[data-testid=list]", "Tuesday")
+      .click()
+      
+      // Checks for the "--selected" class to be assigned
+      .should("have.class", "day-list__item--selected");
+  });
 });
