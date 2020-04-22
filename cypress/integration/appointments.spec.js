f416a4ca1dac3ea7f0ccd0ad4de5ee00f45efc1e
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-04-22 16:13:31 -0400

    cypress test for editing an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -9,20 +9,32 @@ describe("Appointments", () => {
   it("should book an interview", () => {
 
     // Fill out a new interview form and click save
-    cy.get("[alt=Add]")
-      .first()
-      .click();
+    cy.get("[alt=Add]").first().click();
 
     cy.get("[data-testid=student-name-input]")
-      .type("Lydia Miller-Jones")
-      .get("[alt='Sylvia Palmer']")
-      .click();
+      .type("Lydia Miller-Jones");
+    cy.get("[alt='Sylvia Palmer']").click();
 
-    cy.contains("Save")
-      .click();
+    cy.contains("Save").click();
 
     // Expect to see the new appointment card with inputted names
     cy.contains(".appointment__card--show", "Lydia Miller-Jones")
       .contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  it("should edit an interview", () => {
+
+    // Edit the first interview and click save
+    cy.get("[alt=Edit]").first().click({ force: true });
+
+    cy.get("[data-testid=student-name-input]")
+      .clear().type("New Student");
+    cy.get("[alt='Tori Malcolm']").click();
+
+    cy.contains("Save").click();
+
+    // Expect to see the new appointment card with new name
+    cy.contains(".appointment__card--show", "New Student")
+      .contains(".appointment__card--show", "Tori Malcolm");
   });
 })
